8cddd8a8402c83806a89199533bf8fc7c46e4b5b
RakibHossen678 <rh491464@gmail.com>
2024-04-03 22:55:33 +0600

    Header section done

--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,7 +1,12 @@
+import logo from '../../assets/logo.png'
+import moment from 'moment';
 const Header = () => {
     return (
-        <div>
-            <h2>header</h2>
+        <div className='mt-7 flex flex-col justify-center items-center space-y-2'>
+           <img src={logo} alt="" />
+           <p>Journalism Without Fear or Favour</p>
+           <p className='text-2xl'>{moment().format("dddd, MMMM D,YYYY")}</p>
+        
         </div>
     );
 };
